ebfeab12cc6abfdbe52a30135002d2723f9526e3
Chris Lamke <crlamke@gmail.com>
2020-02-23 21:42:31 -0500

    Merge origin/master
    Conflicts:
    	start-page-main.css
    	start-page-main.js
    	start-page.html
    	user-config.txt

--- start-page-main.js
+++ start-page-main.js
@@ -159,3 +159,13 @@ function markTimerSplit() {
 function stopTimer() {
 
 }
+
+var toggler = document.getElementsByClassName("caret");
+var i;
+
+for (i = 0; i < toggler.length; i++) {
+  toggler[i].addEventListener("click", function() {
+    this.parentElement.querySelector(".nested").classList.toggle("active");
+    this.classList.toggle("caret-down");
+  });
+}
